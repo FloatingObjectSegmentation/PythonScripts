eed381d2e3d8ec420625b26e57b972bf45c37e70
mirceta <kristijan.mirceta@gmail.com>
2019-10-09 11:14:59 +0200

    remove option of using labels

--- other/postprocess.py
+++ other/postprocess.py
@@ -28,28 +28,13 @@ for dataset_name in dataset_names:
     # fill up X
     points = [line.rstrip('\n') for line in open(lidar_folder + '\\' + dataset_name + '.txt').readlines()]
     X = np.zeros((len(points), 3))
-    y = {}
     for i in range(len(points)):
         a = points[i].split(' ')
         X[i, :] = np.array([float(a[0]), float(a[1]), float(a[2])])
 
-    # fill up y
-    if use_labels:
-        label_files = [labeling_folder + '\\' + f for f in os.listdir(labeling_folder) if f.find(dataset_name) != -1]
 
-
-        for label_file in label_files:
-
-            lines = [ln.rstrip("\n") for ln in open(label_file, 'r').readlines()]
-
-            label = lines[0]
-            rbnnrad = lines[1]
-            pt_indices = [int(x) for x in lines[2:]]
-
-            y[rbnnrad] = np.zeros(len(points))
-            np.put(y[rbnnrad], np.array(pt_indices), np.ones(len(pt_indices)) * int(label))
-    else:
-        y = np.zeros(len(points))
+    # prepare targets
+    y = np.zeros(len(points))
 
     # load augmentations
     aug_files = [augmentations_folder + '\\' + f for f in os.listdir(augmentations_folder) if f.find(dataset_name) != -1]
@@ -84,36 +69,11 @@ for dataset_name in dataset_names:
             X_augs[curr_idx][:] = np.array([pt[0], pt[1], pt[2]])
             curr_idx += 1
 
-    # fill up y_augs
-    if use_labels:
-        y_augs = {}
-        for rbnn_val in y.keys():
-            curr_idx_of_aug = 0
 
-            y_augs[rbnn_val] = np.zeros(X_augs.shape[0])
-
-            for dist, aug in aug_pts_and_dists:
-                if dist > float(rbnn_val):
-                    for pts in aug:
-                        y_augs[rbnn_val][curr_idx_of_aug] = 1 # make positive
-                        curr_idx_of_aug += 1
-                else:
-                    curr_idx_of_aug += len(aug) # skip this aug
-            # save the results
-        print(X.shape)
-        X = np.concatenate((X, X_augs))  # vertical stack
-        print(X.shape)
-        print(y[rbnn_val].shape)
-        y[rbnn_val] = np.concatenate((y[rbnn_val], y_augs[rbnn_val]))
-        print(y[rbnn_val].shape)
-        X = np.concatenate((X, y[rbnn_val][:, None]), axis=1)  # [:, None] is a trick to transform it to matrix form
-        print(X.shape)
-        np.save(result_savefile + dataset_name + '.npy', X, True, True)
-        print('done')
-    else:
-        y_augs = np.ones(X_augs.shape[0])
-        X = np.concatenate((X, X_augs))
-        y = np.concatenate((y, y_augs))
-        X = np.concatenate((X, y[:, None]), axis=1)
-        np.save(result_savefile + dataset_name + '.npy', X, True, True)
+    # construct final matrix
+    y_augs = np.ones(X_augs.shape[0])
+    X = np.concatenate((X, X_augs))
+    y = np.concatenate((y, y_augs))
+    X = np.concatenate((X, y[:, None]), axis=1)
+    np.save(result_savefile + dataset_name + '.npy', X, True, True)
 
